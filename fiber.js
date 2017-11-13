3ed62fd95216442b16be66e3e0baebb8d3b16f68
Hojimatov <nh019849@m1515010.northamerica.cerner.net>
2017-11-13 12:38:36 -0600

    init commit

---
+++ fiber.js
@@ -0,0 +1,20 @@
+/**
+ * Created by nh019849 on 10/26/17.
+ */
+var fs = require("fs");
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.get('/fibers', function (req, res) {
+  fs.readFile("fiber.json", 'utf8', function (err, data) {
+    res.end(data);
+  });
+});
+
+app.use('/public', express.static(path.join(__dirname + '/public')));
+
+var server = app.listen(8080, function () {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
